8fa2591466b716a18306b9524768b1805d37016b
Wendy Ginsberg <wmginsberg@gmail.com>
2016-09-10 10:25:04 -0400

    update favicon route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static(__dirname + '/'));
+app.use(favicon(__dirname + '/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   			extended: true
